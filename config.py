1c2b489204b178bef30ee7434065f991f698937a
isakura132 <87240191+isakura132@users.noreply.github.com>
2022-04-17 18:51:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,1 @@
-API_TOKEN = '5373941668:AAFiRrAYQvDR5_tDDyILtfyDowSAxB3_45A' # токен от тлеграм бота
-admin = 986023905 # id админа, тобиж твой (взять тут - t.me/userinfobot)
+API_TOKEN = '5373941668:AAFiRrAYQvDR5_tDDyILtfyDowSAxB3_45A' 
